5118db302357749bda2fc3474f601fb5ab8ef0fc
REHMAN KHAN <rehmankhan021295@gmail.com>
2024-10-21 11:12:45 +0530

    Merge branch 'main' of https://github.com/Rehman0212/KatalonGitDemo.git into main

---
+++ Test Suites/OrangeHRMTestSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>OrangeHRMTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1bb69a25-aa06-4ec9-96f1-6527832ddb0f</testSuiteGuid>
+</TestSuiteEntity>
